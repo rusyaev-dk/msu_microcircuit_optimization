0fd475f02f22eb3bb476109e1d8fcfa029ef0d31
Dnrvv_asus <rusyaev.dk@gmail.com>
2024-10-04 18:12:28 +0500

    feat: remove vector field from Stack realization

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 
 int main() {
     try {
-        Stack<int> myStack{};
-        for (int i = 0; i < 100; i++) {
+        StackTest<int> myStack;
+        for (int i = 0; i < 10; i++) {
             myStack.push(i);
         }
-        for (int i = 0; i < 100; i++) {
-            std::cout << myStack.peek();
-            myStack.pop();
+        for (int i = 0; i < 10; i++) {
+            int peekA = myStack.peek();
+            std::cout << peekA;
+            std::cout << myStack.pop();
         }
-    } catch (StackException& e) {
-        std::cout << e.what() << std::endl;
+    } catch (const StackException& e) {
+        std::cout << "Error: " << e.what() << std::endl;
     }
 
     return 0;
--- stack.hpp
+++ stack.hpp
@@ -1,19 +1,18 @@
 #pragma once
-#include <vector>
 
 #include "stack_exception.hpp"
 
-template <typename T = int>
-class Stack {
-    std::vector<T> _data;
+template <typename T>
+class StackTest {
+    T* _data;
     int _stackDepth;
     int _stackSize;
 
     inline bool _isEmpty() const { return this->_stackSize == 0; };
 
    public:
-    Stack();
-    Stack(const std::vector<T>& data);
+    StackTest();
+    StackTest(const StackTest<T>& other);
 
     void push(T elem);
     T pop();
@@ -21,5 +20,56 @@ class Stack {
 
     inline int size() const { return this->_stackSize; }
 
-    ~Stack();
+    ~StackTest();
 };
+
+static const int _DEFAULT_STACK_DEPTH = 100;
+
+template <typename T>
+StackTest<T>::StackTest()
+    : _stackDepth(_DEFAULT_STACK_DEPTH), _stackSize(0) {
+    this->_data = new T[_stackDepth];
+}
+
+template <typename T>
+StackTest<T>::StackTest(const StackTest<T>& other)
+    : _stackDepth(other._stackDepth), _stackSize(other._stackSize) {
+    this->_data = new T[_stackDepth];
+    for (int i = 0; i < _stackSize; ++i) {
+        _data[i] = other._data[i];
+    }
+}
+
+template <typename T>
+void StackTest<T>::push(T elem) {
+    if (_stackSize >= _stackDepth) {
+        throw StackException("Stack overflow. Cannot push more elements.");
+    }
+    _data[_stackSize] = elem;
+    this->_stackSize++;
+}
+
+template <typename T>
+T StackTest<T>::pop() {
+    if (this->_isEmpty()) {
+        throw StackException("Pop() operation aborted: stack is empty.");
+    }
+    T elem = this->_data[this->_stackSize - 1];
+    this->_stackSize--;
+    return elem;
+}
+
+template <typename T>
+T StackTest<T>::peek() const {
+    if (this->_isEmpty()) {
+        throw StackException("Peek() operation aborted: stack is empty.");
+    }
+    return this->_data[this->_stackSize - 1];
+}
+
+template <typename T>
+StackTest<T>::~StackTest() {
+    if (this->_data) {
+        delete[] _data;
+    }
+}
