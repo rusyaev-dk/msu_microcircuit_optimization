5b804b35233b0db9aeac1b18a81848da389b244b
Dnrvv_asus <rusyaev.dk@gmail.com>
2025-02-16 12:27:04 +0500

    refactor: remove unnecessary files

---
+++ out/build/gccwin/CMakeFiles/microcircuit.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ASUS/Desktop/Discrete optimization/microcircuit/logger.cpp" "CMakeFiles/microcircuit.dir/logger.cpp.obj" "gcc" "CMakeFiles/microcircuit.dir/logger.cpp.obj.d"
+  "C:/Users/ASUS/Desktop/Discrete optimization/microcircuit/main.cpp" "CMakeFiles/microcircuit.dir/main.cpp.obj" "gcc" "CMakeFiles/microcircuit.dir/main.cpp.obj.d"
+  "C:/Users/ASUS/Desktop/Discrete optimization/microcircuit/mic_tester.cpp" "CMakeFiles/microcircuit.dir/mic_tester.cpp.obj" "gcc" "CMakeFiles/microcircuit.dir/mic_tester.cpp.obj.d"
+  "C:/Users/ASUS/Desktop/Discrete optimization/microcircuit/microcircuit.cpp" "CMakeFiles/microcircuit.dir/microcircuit.cpp.obj" "gcc" "CMakeFiles/microcircuit.dir/microcircuit.cpp.obj.d"
+  "C:/Users/ASUS/Desktop/Discrete optimization/microcircuit/timer.cpp" "CMakeFiles/microcircuit.dir/timer.cpp.obj" "gcc" "CMakeFiles/microcircuit.dir/timer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/build/gccwin/CMakeFiles/microcircuit.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/microcircuit.dir/logger.cpp.obj"
+  "CMakeFiles/microcircuit.dir/logger.cpp.obj.d"
+  "CMakeFiles/microcircuit.dir/main.cpp.obj"
+  "CMakeFiles/microcircuit.dir/main.cpp.obj.d"
+  "CMakeFiles/microcircuit.dir/mic_tester.cpp.obj"
+  "CMakeFiles/microcircuit.dir/mic_tester.cpp.obj.d"
+  "CMakeFiles/microcircuit.dir/microcircuit.cpp.obj"
+  "CMakeFiles/microcircuit.dir/microcircuit.cpp.obj.d"
+  "CMakeFiles/microcircuit.dir/timer.cpp.obj"
+  "CMakeFiles/microcircuit.dir/timer.cpp.obj.d"
+  "libmicrocircuit.dll.a"
+  "microcircuit.exe"
+  "microcircuit.exe.manifest"
+  "microcircuit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/microcircuit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
